abda6395589e0dc27b4b2b48ab97cd7e3fe9f15c
Viwu <sinconsistencia@gmail.com>
2025-12-12 12:25:58 -0600

    folder

--- source/ScenePlayer.hx
+++ source/ScenePlayer.hx
@@ -1,3 +1,13 @@
 import flixel.FlxState;
 
-class ScenePlayer extends FlxState {}
+class ScenePlayer extends FlxState
+{
+    public var folder:String = '';
+
+	override public function new(folder:String)
+	{
+		super();
+
+        this.folder = folder;
+	}
+}
